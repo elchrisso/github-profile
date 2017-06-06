ab76e7def8b9bc26553f529db91c17581bb08e7e
elchrisso <elchrisso@hotmail.com>
2017-06-06 15:07:11 -0600

    initial commit

---
+++ src/ProjectBox.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class ProjectBox extends React.Component {
+  render() {
+    return(
+      <div className="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>...</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+      </div>
+    )
+  }
+
+}
+
+export default ProjectBox
